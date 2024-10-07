bf6c7b9f7a7a53ffc2b2b3f07d69f006384eb20e
Rashmi Salunke <salunkers213@gmail.com>
2024-10-07 21:52:22 +0530

    For loop

--- src/basicJava/OCT07_JavaVariable.java
+++ src/basicJava/OCT07_JavaVariable.java
@@ -1,5 +1,13 @@
 package basicJava;
 
 public class OCT07_JavaVariable {
+public static void main(String[] args) {
+	
 
+	for(int i=0; i<10; i++) {
+		System.out.println(i);
+		System.out.println();
+	}
+}
+	
 }
